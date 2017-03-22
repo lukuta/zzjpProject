b9f6e031c623bfc0fcc49318621c1651bd5bbb7c
lkuta <kutalukasz@gmail.com>
2017-03-22 17:00:58 +0100

    init commit

---
+++ src/main/java/com/lkuta/DartApplication.java
@@ -0,0 +1,12 @@
+package com.lkuta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DartApplication.class, args);
+	}
+}
